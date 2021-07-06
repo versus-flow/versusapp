0b8c9b9870529f95ed202921b5e4471dc8dcd954
rubenpa <rubenpa321@gmail.com>
2021-07-06 12:12:06 -0700

    small change

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 import Nav from "../components/general/Nav";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
+import * as ga from "../components/general/ga";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />;
 }
 
