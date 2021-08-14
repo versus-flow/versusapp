16622744f0f81a37f99d27b43bf9b4618f44137e
rubenpa <rubenpa321@gmail.com>
2021-08-14 14:12:25 +0900

    empty prompt

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       {() => (
         <>
           <Landing />
-          <MarketplacePreview />
+          {/* <MarketplacePreview /> */}
           <Rewarded />
           <BetterForArt />
         </>
