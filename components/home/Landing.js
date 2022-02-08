795b634282901478a8a0d20a404cd58cf7c05972
ruben <rubenpa321@gmail.com>
2022-02-08 00:01:44 -0600

    drop 54

--- components/home/Landing.js
+++ components/home/Landing.js
@@ -31,23 +31,21 @@ const Landing = ({ drop }) => {
       <div className="fd-section bg-cream-500 z-10 relative sm:py-6">
         <div className="container flex flex-col-reverse sm:gap-16 grid-cols-2 hero-height items-center relative sm:grid z-10">
           <div className="h-full flex flex-col justify-center sm:items-start sm:py-0 text-center sm:text-left">
-            <h3 className="-backdrop-hue-rotate-15 font-inktrap text-xl">
-              {drop.metadata.artist} multi-part auction
-            </h3>
             {/* <h3 className="-backdrop-hue-rotate-15 font-inktrap text-xl">
-              {drop.metadata.artist}
+              {drop.metadata.artist} multi-part auction
             </h3> */}
+            <h3 className="-backdrop-hue-rotate-15 font-inktrap text-xl">
+              {drop.metadata.artist}
+            </h3>
             <h2 className="2xl:text-8xl font-bold font-inktrap leading-loose lg:text-7xl relative sm:-left-1 sm:text-5xl text-3xl">
               {drop.metadata.name}
             </h2>
             <div className="mt-4 sm:mt-8">
               <h4 className="font-inktrap font-semibold tracking-wide">
                 {timer
-                  ? `The first auction starts ${start.format(
-                      "MMMM"
-                    )} ${start.format("Do")} at ${start
-                      .utcOffset(-5)
-                      .format("hA")} EST`
+                  ? `The auction starts ${start.format("MMMM")} ${start.format(
+                      "Do"
+                    )} at ${start.utcOffset(-5).format("hA")} EST`
                   : moment().unix() - parseInt(drop.endTime, 10) < 0
                   ? "Auction now open!"
                   : "Auction finished!"}
